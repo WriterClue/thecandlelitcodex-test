421bd855b15e13e5e60f1088c8fc1bc2e6cdff64
Clue Encilis <thecandlelitcodex@gmail.com>
2025-06-23 23:42:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ window.addEventListener('load', () => {
     loader.style.display = 'none';
   }
 });
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const loader = document.getElementById('loader-wrapper');
+    if (loader) {
+      loader.style.display = 'none';
+    }
+  }, 5000); // 5 seconds delay
+});
