96eddad9b7c6aab91c752c300f7874c43b4d8a62
Clue Encilis <thecandlelitcodex@gmail.com>
2025-06-23 23:23:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const loader = document.getElementById('loader-wrapper');
     setTimeout(() => {
       loader.style.display = 'none';
-    }, 20000); // 20 seconds
+    }, 10000); // 20 seconds
   });
 <script>
