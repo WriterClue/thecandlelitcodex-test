5fe317717fc3cfd95da3d12ef4a35bca4575eb69
Clue Encilis <thecandlelitcodex@gmail.com>
2025-06-23 21:16:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const header = document.querySelector('.contact-card');
 
   window.addEventListener('scroll', function () {
-    if (window.scrollY > 3) {
+    if (window.scrollY > 100) {
       header.classList.add('shrink');
     } else {
       header.classList.remove('shrink');
